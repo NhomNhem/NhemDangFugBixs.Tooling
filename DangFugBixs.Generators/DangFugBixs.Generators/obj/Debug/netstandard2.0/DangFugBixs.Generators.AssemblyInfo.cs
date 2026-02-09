4697947bacd92a871b57081de9b1c6221d87a1d3
NhemNhem <95702943+NhomNhem@users.noreply.github.com>
2026-02-09 16:55:03 +0700

    Refactor: Convert Tooling project to UPM Package structure

--- DangFugBixs.Generators/DangFugBixs.Generators/obj/Debug/netstandard2.0/DangFugBixs.Generators.AssemblyInfo.cs
+++ DangFugBixs.Generators/DangFugBixs.Generators/obj/Debug/netstandard2.0/DangFugBixs.Generators.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DangFugBixs.Generators")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0c7823ddd494914961ca33c1d684ae59f828c63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39f27e65e61fed7e02f90a1485ba9a885947885c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DangFugBixs.Generators")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DangFugBixs.Generators")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
