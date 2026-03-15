e4d43ac101a50c03515afcf5968b4ed75a51fc8d
NhemNhem <95702943+NhomNhem@users.noreply.github.com>
2026-03-16 02:44:39 +0700

    refactor: SG

---
+++ Source~/DangFugBixs.Runtime~/ILifetimeScopeIdentity.cs
@@ -0,0 +1,9 @@
+namespace NhemDangFugBixs.Attributes {
+    /// <summary>
+    /// Optional marker interface for Identity Types.
+    /// Using this is not required, but it provides a clear intention that a type 
+    /// is being used as a VContainer Scope Identity.
+    /// </summary>
+    public interface ILifetimeScopeIdentity {
+    }
+}
