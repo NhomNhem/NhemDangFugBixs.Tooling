4e92bd2966aad7049a56984f4f89e131b47916e8
NhemNhem <truongduy18.03.03.al@gmail.com>
2026-06-01 09:46:12 +0700

    fix: use non-generic syntax for LifetimeScopeFor in test

--- Source~/DangFugBixs.Analyzers~/DangFugBixs.Analyzers.Tests/ArchitectureGuardrailsRuleTests.cs
+++ Source~/DangFugBixs.Analyzers~/DangFugBixs.Analyzers.Tests/ArchitectureGuardrailsRuleTests.cs
@@ -30,10 +30,10 @@ namespace NhemDangFugBixs.Attributes {
         var test = """
 using NhemDangFugBixs.Attributes;
 public interface IGameplayScope { }
-[LifetimeScopeFor<IGameplayScope>] public class {|#0:A|} { }
-[LifetimeScopeFor<IGameplayScope>] public class {|#1:B|} { }
+[LifetimeScopeFor(typeof(IGameplayScope))] public class {|#0:A|} { }
+[LifetimeScopeFor(typeof(IGameplayScope))] public class {|#1:B|} { }
 namespace NhemDangFugBixs.Attributes {
-  public class LifetimeScopeForAttribute : System.Attribute { public LifetimeScopeForAttribute() {} }
+  public class LifetimeScopeForAttribute : System.Attribute { public LifetimeScopeForAttribute(System.Type t) {} }
 }
 """;
         var e0 = Verifier.Diagnostic("NDFG011").WithLocation(0).WithArguments("IGameplayScope");
